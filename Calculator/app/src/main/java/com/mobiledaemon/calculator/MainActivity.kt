4f1f50a75bfd4f7c5e734facc04e968f8a94c52e
Jishnu-Nandhiath <jishnun789@gmail.com>
2022-04-26 01:27:52 +0530

    Calculator Project UI Completed

--- Calculator/app/src/main/java/com/mobiledaemon/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/mobiledaemon/calculator/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mobiledaemon.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var userInputTextView : TextView = findViewById(R.id.userInput)
+
+    var userInputText : String? =
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
 }
